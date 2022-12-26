19dc5945cd87fb86697c8f3f84bd4bbb62757952
Diego Rey <holacinema@hotmail.com>
2022-12-26 13:13:02 -0300

    funcionando desencriptar

--- script.js
+++ script.js
@@ -23,23 +23,30 @@ const encriptar = (input) => {
 
 const desencriptar = (input) => {
     texto = [];
-    for (n in input) {
-        while (input(n + 5) != null) {
-            if (input[n] == "a")
-                texto[n] = "ai";
-            else if (input[n] == "e")
-                texto[n] = "enter";
-            else if (input[n] == "i")
-                texto[n] = "imes";
-            else if (input[n] == "o")
-                texto[n] = "ober";
-            else if (input[n] == "u")
-                texto[n] = "ufat";
+    for (n = 0; n<input.length; n++) {
+            if (input[n] == "a" && input[n+1] == "i"){
+                texto.push("a");
+                n= n+1;
+            }
+            else if (input[n] == "e" && input[n+1] == "n" && input[n+2] == "t" && input[n+3] == "e" && input[n+4] == "r") {
+                texto.push("e");
+                n = n+4;
+            }
+            else if (input[n] == "i" && input[n+1] == "m" && input[n+2] == "e" && input[n+3] == "s") {
+                texto.push("i");
+                n = n+3;
+            }
+            else if (input[n] == "o" && input[n+1] == "b" && input[n+2] == "e" && input[n+3] == "r"){
+                texto.push("o");
+                n = n+3;
+            }
+            else if (input[n] == "u" && input[n+1] == "f" && input[n+2] == "a" && input[n+3] == "t"){
+                texto.push("u");
+                n = n+3;
+            }
             else
-                texto[n] = input[n];
+                texto.push(input[n]);
         }
-
-    }
     return texto.join("");
 }
 
